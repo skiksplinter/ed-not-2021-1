ac440444d4634e30f089571456b82baac80ec9c8
skiksplinter <skiksplinter@hotmail.com>
2021-06-11 01:25:27 +0000

    comentários das alterações

--- ex-estruturas01.mjs
+++ ex-estruturas01.mjs
@@ -2,30 +2,30 @@
     Utilize a estrutura de dados adequada e implemente um programa que converta
     um número decimal em um número binário.
 
-    Data de entrega: 31/05, até 20h50, valendo nota de participação
+    Data de entrega: 31/02, até 20h50, valendo nota de participação
 */
 
-function decForBin(NumDec) {
-    
-    var restPilha = [], //pilha de restos
-      rest, //variável de resto
-      binString = ''; //string de binários inicializada como vazio
-  
-    //enquanto o numero decimal for maior que zero
-    while (NumDec > 0) {
-      //res recebe arredondamento pra baixo, o resto de NumDec/2
-      //é maior que 0?
-      rest = Math.floor(NumDec % 2);
-      restPilha.push(rest); 
-      NumDec = Math.floor(NumDec / 2); //sempre arredonda pra baixo
-    } // volta no loop até ficar 0
-  
-    while (restPilha.length > 0) {
-      // tamanho da pilha é maior que 0?
-      binString += restPilha.pop().toString(); // recebe cumulativamente o ultimo valor
-    } //convertido em string
-    return binString; // retorna com os resultados invertido
-  }
-  
-  console.log(decForBin(100));
-  
+// Usaremos a estutura pilha
+import { Stack } from './lib/Stack.mjs'
+
+const numDecimal = 2021
+
+let num = numDecimal
+
+const restos = new Stack()
+
+while(num > 0) {
+    restos.push(num % 2)        // Empilha o resto da divisão do número por 2
+    // Corta o número pela metade, desprezando as casas decimais
+    num = Math.floor(num / 2)
+}
+
+console.log(restos.print())
+
+let binario = ''
+
+while(! restos.empty) {
+    binario += restos.pop()
+}
+
+console.log(`${numDecimal} em base 10 equivale a ${binario} em base 2.`)
